d10d3f731ab2488ea71965c966b6fd353feedadd
David <davidafarmer@gmail.com>
2023-03-20 12:28:29 -0400

    Phase1 Arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 10;
+const num4 = 6
+const mod = num3 % num4;
+
+const numbers = [2, 7, 20, 6, 10];
+const max = Math.max(...numbers);
+
